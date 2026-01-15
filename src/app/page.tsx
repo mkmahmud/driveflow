d8c17b70c61866ffb98f78ffe263ca1aba6a82fd
mkmahmud <mahmudulhasanmk434@gmail.com>
2026-01-16 02:19:40 +0600

    added layout and hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,14 @@
 "use client";
 
-import { trpc } from '@/trpc/client';
+import Hero from '@/components/sections/home/hero';
+ 
 
 export default function Home() {
-  const cars = trpc.car.getAll.useQuery();
 
-  if (cars.isLoading) return <div>Loading cars...</div>;
-  if (cars.error) return <div>Error loading cars: {cars.error.message}</div>;
-  if (cars) console.log(cars.data);
 
   return (
-    <main className="p-8">
-      <h1 className="text-2xl font-bold mb-4">Available Cars</h1>
-      <div className="grid gap-4">
-        {
-          cars.data && cars.data
-        }
-      </div>
-    </main>
+    <div className="">
+      <Hero />
+    </div>
   );
 }
